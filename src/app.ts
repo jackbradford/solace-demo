317cb419c66145d10c6a332ef23aa2e43e5cc3b3
Jack Brown <jack@jackbrown.io>
2023-08-10 15:20:30 -0700

    Added htdocs -- exported from solace-demo-ui. Serve UI at /* except /api*.

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,20 @@ import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import middlewares from './middlewares';
 import { routes } from './routes';
+import path from 'path';
 
 const app = express();
 const port = 3003;
 app.use(middlewares);
-
-app.get('/', (req: Request, res: Response) => {
-  res.send("Done."); // FIXME: deliver FE.
-});
+app.use(express.static(process.cwd() + '/htdocs'));
 
 for (const route of Object.values(routes)) {
   app[route.method](`/api${route.path}`, route.endpoint);
 }
+
+app.get('/*', (req: Request, res: Response) => {
+  res.sendFile(`${process.cwd()}/htdocs/index.html`);
+});
 
 app.listen(port, () => {
   return console.log(`Listening: http://localhost:${port}.`);
